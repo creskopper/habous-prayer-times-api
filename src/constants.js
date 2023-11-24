9f5eefb23426a0b9928f4b13b93d1f89b6cfa592
Cresk <109740836+creskopper@users.noreply.github.com>
2023-11-24 17:43:42 +0100

    Added Arabic to English mapping for Gregorian months

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,18 @@ export const HABOUS_AR_PRAYER_TIMES_URL =
 	"https://habous.gov.ma/prieres/horaire_hijri_2.php";
 export const HABOUS_FR_PRAYER_TIMES_URL =
 	"https://www.habous.gov.ma/prieres/horaire_hijri_fr.php";
+
+export const GREGORIAN_MONTHS_ARABIC_TO_ENGLISH = {
+	يناير: { index: 0, name: "January" },
+	فبراير: { index: 1, name: "February" },
+	مارس: { index: 2, name: "March" },
+	أبريل: { index: 3, name: "April" },
+	ماي: { index: 4, name: "May" },
+	يونيو: { index: 5, name: "June" },
+	يوليوز: { index: 6, name: "July" },
+	غشت: { index: 7, name: "August" },
+	سبتمبر: { index: 8, name: "September" },
+	أكتوبر: { index: 9, name: "October" },
+	نوفمبر: { index: 10, name: "November" },
+	ديسمبر: { index: 11, name: "December" },
+};
