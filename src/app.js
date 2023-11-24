bffe1296b987b8cb06512746f3a956b09fbc9ac6
Cresk <109740836+creskopper@users.noreply.github.com>
2023-11-24 15:43:16 +0100

    Implement available-cities route using Cheerio to extract cities from habous website

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import express, { json } from "express";
 
+import AvailableCitiesRoute from "./routes/available-cities.js";
+
 const app = express();
 
 app.use(json());
+
+app.use("/api/v1/", AvailableCitiesRoute);
 
 app.get("/", (req, res) => {
 	res.send({
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const HABOUS_AR_PRAYER_TIMES_URL =
+	"https://habous.gov.ma/prieres/horaire_hijri_2.php";
+export const HABOUS_FR_PRAYER_TIMES_URL =
+	"https://habous.gov.ma/fr/horaires-de-pri%C3%A8re.html";
