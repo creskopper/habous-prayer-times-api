9f84227f9af0dd5afccaa4416d066cee073e935c
Cresk <109740836+creskopper@users.noreply.github.com>
2023-11-24 15:53:26 +0100

    Change Habous frensh prayer times url the previous url includes prayer data inside an iFrame.

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const HABOUS_AR_PRAYER_TIMES_URL =
 	"https://habous.gov.ma/prieres/horaire_hijri_2.php";
 export const HABOUS_FR_PRAYER_TIMES_URL =
-	"https://habous.gov.ma/fr/horaires-de-pri%C3%A8re.html";
+	"https://www.habous.gov.ma/prieres/horaire_hijri_fr.php";
