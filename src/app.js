e1f5d12509ca38673bd0a879360a8c99320750ab
Cresk <109740836+creskopper@users.noreply.github.com>
2023-11-25 21:58:11 +0100

    Implemented CORS middleware to enable cross-origin requests

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express, { json } from "express";
 import path from "path";
+import cors from "cors";
 
 import AvailableCitiesRoute from "./routes/available-cities.js";
 import PrayerTimesRoute from "./routes/prayer-times.js";
@@ -8,6 +9,7 @@ const app = express();
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
+app.use(cors());
 app.use(json());
 app.use(express.static(path.join(__dirname, "public")));
 
